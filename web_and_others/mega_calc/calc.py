c997088514c2564f9714bd5644406f1017df6c56
Maxim Vostretsov <47358715+Moxi43@users.noreply.github.com>
2021-04-12 16:32:23 +0400

    delete qml

--- web_and_others/mega_calc/calc.py
+++
@@ -1,7 +0,0 @@
-from PyQt5 import QTWidgets
-from ui_calculator import Ui_Calculator
-
-class CalculatorWindow(QTWidgets, QMainWindow, Ui_calculator):
-	def __init__(self):
-		super().__init__()
-		self.show()
--- web_and_others/mega_calc/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from calc import CalculatorWindow
-
-app = QApplication(sys.argv)
-
-calculator = CalculatorWindow()
-
-sys.exit(app.exec())
